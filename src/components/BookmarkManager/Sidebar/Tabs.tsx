9fa86e438521a8782cad85b4dfff56b7fc219fc6
Neeraj Bhardwaj <neerajb@yahoo.com>
2023-03-15 01:23:31 +0530

    tabs container bg color changed

--- src/components/BookmarkManager/Sidebar/Tabs.tsx
+++ src/components/BookmarkManager/Sidebar/Tabs.tsx
@@ -4,7 +4,7 @@ import Tab from './Tab'
 const Tabs = () => {
   return (
     <div
-      className={`w-full min-h-[32px] border border-dashed border-blue-400 p-1`}
+      className={`w-full min-h-[32px] border border-dashed border-blue-400 p-1 bg-blue-400 bg-opacity-40`}
     >
       <Tab />
     </div>
