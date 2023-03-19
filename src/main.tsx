801c1ccb86334aa39c3df6cce74a1b27b27f55fa
Neeraj Bhardwaj <neerajb@yahoo.com>
2023-03-19 13:42:53 +0530

    react dnd context removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { DndProvider } from 'react-dnd'
 import ReactDOM from 'react-dom/client'
-import { HTML5Backend } from 'react-dnd-html5-backend'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
-      <App />
-    </DndProvider>
+    <App />
   </React.StrictMode>
 )
